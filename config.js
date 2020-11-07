8a4a371de1431c90a1815253feb4fd0ddaa13d2f
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-07 14:28:24 -0500

    Week 4 Assignment

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/nucampsite'
+  mongoUrl: 'mongodb://localhost:27017/nucampsite',
+  facebook: {
+    clientId: '351998162557173',
+    clientSecret: 'faa796aeca184365d5ebde2bd926fdf8'
+  }
 };
